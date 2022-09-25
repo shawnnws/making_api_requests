4ce94cac91283ea225b6195bb60d5c7a8520bc10
shawnng <shawnngweisong@gmail.com>
2022-09-25 10:29:20 +0800

    feat(amend-client): amend client according to server

--- main.py
+++ main.py
@@ -1,19 +1,48 @@
+import json
 from typing import Dict
 import requests
 from requests import Response
+from pydantic import BaseModel
 
 
-def get_call_option_price() -> Dict[str, float]:
+class CallOptionPrice(BaseModel):
+    stock: str # the field name is case sensitive
+    price: float
+
+
+# def get_call_option_price() -> Dict[str, float]:
+def get_call_option_price() -> CallOptionPrice:
+
+    """
+    Makes a GET request to our call option price predictor service
+    """
     url: str = "http://localhost:6000/call_option_price"
+
+    data: Dict[str, str] = {
+        "stock": "AAPL"
+    }
+
+    # GET, POST, PUT, DELETE, UPDATE
+    # TLDR: people normally only use GET and POST
+    # On the server, we are getting the "data" field  from the request; this means
+    # we need to pass in the input data (data), under the "data" field, instead of under params
     response: Response = requests.get(
-        url=url
+        url=url,
+        data=json.dumps(data)
     )
     # to check for type hints: print the type(object)
     # print(f"response: {type(response)}")
     response_json: Dict[str, float] = response.json()
-    return response_json
+
+    # converting a dictionary into a data class; pydantic.BaseModel
+    call_option_price: CallOptionPrice = CallOptionPrice.parse_obj(response_json)
+    return call_option_price
 
 
 if __name__ == "__main__":
-    call_option_price: Dict[str, float] = get_call_option_price()
+    call_option_price: CallOptionPrice = get_call_option_price()
+
+    # Test: pydantic.BaseModel provides type safety; it doesn't allow us to pass the wrong type to the field
+    # call_option_price: CallOptionPrice = CallOptionPrice(stock="AAPL", price="100-1")
+
     print(call_option_price)
